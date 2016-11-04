5bbbeaf8ab4c4e10eb9af9288b67c7f2e43522d2
THACHER <thachert1d@gmail.com>
2016-11-04 11:52:18 -0700

    student version working

--- app/app.js
+++ app/app.js
@@ -1,16 +1,4 @@
 import angular from 'angular';
 import angularMaterialize from 'angular-materialize';
-import uiRouter from 'angular-ui-router';
 
-angular.module('my-app', [angularMaterialize, uiRouter])
-  .config(['$stateProvider', ($stateProvider) => {
-    $stateProvider
-      .state('home', {
-        url: '',
-        templateUrl: 'views/main.html'
-      })
-      .state('main', {
-        url: '/',
-        templateUrl: 'views/main.html'
-      });
-  }]);
+angular.module('my-app', [angularMaterialize])
